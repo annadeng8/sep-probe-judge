d8f6e4322ce7b4e91a1c9a9bcbd6da6749213063
Adithya Gnanasundar <147122594+adithyagsundar@users.noreply.github.com>
2025-07-17 16:43:18 -0700

    Update utils.py

--- uncertainty/utils/utils.py
+++ uncertainty/utils/utils.py
@@ -22,5 +22,5 @@ def get_parser():
     parser.add_argument("--model_name", type=str, default="google/gemma-2-2b", help="Model name")
     parser.add_argument("--model_max_new_tokens", type=int, default=100, help="Max tokens to generate")
     parser.add_argument("--num_samples", type=int, default=400, help="Number of samples to process")
-    parser.add_argument("--temperature", type=float, default=0.6, help="Sampling temperature")
+    parser.add_argument("--temperature", type=float, default=1.0, help="Sampling temperature")
     return parser
